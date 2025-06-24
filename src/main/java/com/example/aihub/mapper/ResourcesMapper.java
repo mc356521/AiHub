ca64d4b272a067362e7dece1d97c0ac03357478c
MC <2281034748@qq.com>
2025-06-24 16:18:27 +0800

    新增ResourcesMapper接口，继承自BaseMapper以支持资源实体的数据库操作。

---
+++ src/main/java/com/example/aihub/mapper/ResourcesMapper.java
@@ -0,0 +1,8 @@
+package com.example.aihub.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.aihub.entity.LearningTasks;
+import com.example.aihub.entity.Resources;
+
+public interface ResourcesMapper  extends BaseMapper<Resources> {
+}
